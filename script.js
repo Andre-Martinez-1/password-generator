e2f1690f0ba116d2ad3318032286a2e59e901efb
Andre Martinez <andre_martinez15@yahoo.com>
2022-08-19 21:45:02 -0700

    adding Javascript variables

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
 // Assignment code here
 
+var lowercase = "abcdefghijklmnopqrstuvwxyz".split("");
+var uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split("");
+var specialchar = ["!","\u0022","#","$","%","&","'","(",")","*","+",",","-",
+".","/",":",";","<","=",">","?","@","[","\u005C","]","^","_","`","{",
+"|", "}","~",];
+var numbers = "0123456789".split("");
+
+// [] will hold arrays
+var ultronArray = [];
+
+function generatePassword(){
+
+  var userLength = prompt("How long would you like your password? \n Select a number between 8 and 128!");
+  var userLower = confirm("Would you like any lowercase letters? \n Select yes to continue, no to cancel");
+  var userUpper = confirm("Would you like any uppercase letters? \n Select yes to continue, no to cancel");
+  var userSpecial = confirm("Would you like any special characters? \n Select yes to continue, no to cancel");
+  var userNumber = confirm("Would you like any numbers? \n Select yes to continue, no to cancel");
+
+}
+
+
+
+
+
 
 // Get references to the #generate element
 var generateBtn = document.querySelector("#generate");
@@ -15,3 +39,12 @@ function writePassword() {
 
 // Add event listener to generate button
 generateBtn.addEventListener("click", writePassword);
+
+
+/* Password has to have lower upper numbers special char Variables
+You want the generate button to start, do you want lower, upper, etc
+IF they want uppercase, add to password possiblility,
+want an empty array to hold the user choices
+
+After that, make the user chose the desired length o
+*/
